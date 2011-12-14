fdae89122d3124e2efbbdcb62a8868914ecbad55
rboo <gokcetaskan@gmail.com>
2011-12-14 02:27:21 +0200

    modifications, some things are still not working but getting there...

--- website/js/script.js
+++ website/js/script.js
@@ -1,21 +1,27 @@
 // introducing a reuna namespace to contain our util functions
 var reuna = {};
 
-$(function() {
+$(document).ready(function() {
   window_width = 0;
   window_height = 0;
+  window_width = 0;
+  current_section = 0;
+  total_sections = 0;
+  current_carousel = [];
+  total_carousels = [];
   
   resizeHandler = function() {
     window_width = $(window).width();
     window_height = $(window).height();
-    $('#content div').css({'height':window_height,'width':window_width});
-    //$('html,body').animate({scrollTop:window_height*current_section},0);
+    $('.section').css({'height':window_height,'width':window_width});
+    $('html,body').animate({scrollTop:window_height*current_section},0);
     scaleCarouselImages();
     scaleCarouselVideos();
+    placeCarouselDivs();
   }
   
   $('#info').click(function() {
-		$('.contenutext').delay(50).animate({width:'toggle'}, 300);
+		$('.carouseltext').delay(50).animate({width:'toggle'}, 300);
 	});
   
   $(window).load(function() { scaleCarouselImages(); });
@@ -23,32 +29,102 @@ $(function() {
   init = function() {
       $(window).resize(resizeHandler);
       resizeHandler();
+      $('ul#nav').find('li').each(function(k,v){
+              total_sections++;
+              $(this).click(function(){
+                //current_section = k;
+                //$('html,body').animate({scrollTop:window_height*k},'slow');
+                gotoSection(k);
+              });
+            });
+            $(window).keyup(keyDown);
+            /*
+            $('.carousel').cycle({
+            		fx: 'fade',
+            		timeout:10000,
+            	});*/
+      $('.carousel').each(function(k,v) {
+        current_carousel[k] = 0;
+        total_carousels[k] = 0;
+        $(this).find('>div').each(function(){total_carousels[k]+=1;});
+      });
       setVimeoReadyEvents();
   }
   
+  keyDown = function(e) {
+      switch(e.keyCode) {
+        case 37:
+          //prev carousel
+          prevCarousel();
+          break;
+        case 39:
+          //next carousel
+          nextCarousel();
+          break;
+        case 38:
+          prevSection();
+          break;
+        case 40:
+          nextSection();
+          break;
+      }
+    }
+    prevCarousel = function() {
+      if (current_carousel[current_section]<=0) {return;}
+      gotoCarousel(current_carousel[current_section]-1);
+    }
+    nextCarousel = function() {
+      if (current_carousel[current_section]>=total_carousels[current_section]-1) {return;}
+      gotoCarousel(current_carousel[current_section]+1);
+    }
+    gotoCarousel = function(i) {
+      current_carousel[current_section] = i;
+      $('.carousel').animate({
+          left: window_width*-i,
+          easing: 'easeOutExpo'}, 400);
+    }
+
+    prevSection = function() {
+      if (current_section<=0) {return;}
+      gotoSection(current_section-1);
+    }
+    nextSection = function() {
+      if (current_section>=total_sections) {return;}
+      gotoSection(current_section+1);
+    }
+    gotoSection = function(i) {
+      if (i>=total_sections) {return;}
+      current_section=i;
+      $('html,body').animate({scrollTop:window_height*i},'slow');
+    }
+  
   scaleCarouselImages = function() {
     /* Custom stuff based on jQuery Backstretch 1.2.4 http://srobbin.com/jquery-plugins/jquery-backstretch/ */
-    $('.scalefit').find('img').each(function(){
+    $('.carousel').find('img').each(function(){
       var self = $(this), imgWidth, imgHeight;
       rootElement = ("onorientationchange" in window) ? $(document) : $(window);
+      var rootWidth = rootElement.width();
+      var rootHeight = rootElement.height();
+      //rootWidth -= 200;
       self.css({width: "auto", height: "auto"});
       imgWidth = this.width;
       imgHeight = this.height;
       imgRatio = imgWidth / imgHeight;
     
-      bgCSS = {left: 0, top: 0}
-      bgWidth = rootElement.width();
+      bgCSS = {left: 0, top: 0};
+      bgWidth = rootWidth;
       bgHeight = bgWidth / imgRatio;
+      $(this).css({position: "absolute"});
     
       // Make adjustments based on image ratio
       // Note: Offset code provided by Peter Baker (http://ptrbkr.com/). Thanks, Peter!
-      if(bgHeight >= rootElement.height()) {
-          bgOffset = (bgHeight - rootElement.height()) /2;
+      if(bgHeight >= rootHeight) {
+          bgOffset = (bgHeight - rootHeight) /2;
           $.extend(bgCSS, {top: "-" + bgOffset + "px"});
       } else {
-          bgHeight = rootElement.height();
+          bgHeight = rootHeight;
           bgWidth = bgHeight * imgRatio;
-          bgOffset = (bgWidth - rootElement.width()) / 2;
+          bgOffset = (bgWidth - rootWidth) / 2;
           $.extend(bgCSS, {left: "-" + bgOffset + "px"});
       }
       $.extend(bgCSS, {visibility: 'visible'});
@@ -56,15 +132,23 @@ $(function() {
       });
   }
   scaleCarouselVideos = function() {
-    $('.contenu').find('iframe').each(function(){
+    $('.carousel').find('iframe').each(function(){
       $(this).width( window_width ).height( window_height );
     });
   }
   placeCarouselText = function() {
-    $('.contenu').find('.contenutext').each(function(){
+    $('.carousel').find('.contenutext').each(function(){
       $(this).css('width', window_width);
       $(this).css('visibility','visible');
     });
+  }
+  placeCarouselDivs = function() {
+    var posx = 0;
+    $('.carousel > div').each(function() {
+      $(this).css({'position':'absolute','left':posx,'overflow':'hidden','width':window_width,'height':window_height});
+       posx += window_width;
+    });
+    $('.carousel').css('width',posx);
   }
   setVimeoReadyEvents = function() {
     var vimeoPlayers = document.querySelectorAll('iframe'), player;
@@ -92,32 +176,16 @@ $(function() {
   }
 
 	init();
+	//$('#nav').animate({width:'hide'}, 0);
+	$('#stuffpage').fadeOut(0);
+  $('#topleftlogo').hover(function(){ $('#nav').stop(true,true).animate({height:'show'}, 200); },function(){  $('#nav').stop(true,true).animate({height:'hide'}, 200); });
+  
+  $("#stuff").click(function(){ $('#stuffpage').delay(100).fadeIn(700); });
+  $("#stuffpage").click(function(){ $('#stuffpage').delay(100).fadeOut(700); });
 });
-
+/*
 $(document).ready(function() {
-  //ascensor
-  $('#content').ascensor({
-	  AscensorName:'page',
-	  WindowsFocus:true,
-	  WindowsOn:1,
-	
-	  NavigationDirection:'xy',
-	  Direction:'y',
-	  Navig:true,
-	  Link:true,
-	  ReturnURL:true,
-	  PrevNext:false,
-	  CSSstyles:false,
-	
-	  KeyArrow:true,
-	  keySwitch:false,
-	  ReturnCode:false,
-	
-	  ChocolateAscensor:true,
-	  AscensorMap:'5|3',
-	  ContentCoord:'1|1 & 2|1 & 3|1 & 3|2 & 3|3 & 4|1 & 4|2 & 4|3 & 5|1 & 5|2'
-  });
-
+  
   $(document).keydown(function(e){
     switch(e.keyCode){
       case 37: //leftkey
@@ -138,14 +206,10 @@ $(document).ready(function() {
   // hide in the beginnning
 	$('#nav').animate({width:'hide'}, 0);
 	$('#stuffpage').fadeOut(0);
-  
-  // show/hide navigation
-  $("#logo").mouseenter(function(){ $('#nav').stop().animate({height:'show'}, 300); }); // show when entering menu
-  $("#nav").mouseenter(function(){ $('#nav').stop().animate({height:'show'}, 300); }); // show when entering menu
-	$("#nav, #logo").mouseleave(function(){ $('#nav').stop().animate({height:'hide'}, 300); }); // hide when leaving menu
+  $('#topleftlogo').hover(function(){ $('#nav').stop(true,true).animate({height:'show'}, 200); },function(){  $('#nav').stop(true,true).animate({height:'hide'}, 200); });
   
   $("#stuff").click(function(){ $('#stuffpage').delay(100).fadeIn(700); });
   $("#stuffpage").click(function(){ $('#stuffpage').delay(100).fadeOut(700); });
   
 });
-  
+  */
