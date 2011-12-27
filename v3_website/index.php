26db3f0e1aa14dad7d66ef8bf43d5999720a41cf
rboo <gokcetaskan@gmail.com>
2011-12-27 18:26:02 +0200

    analytics (disabled in local)

---
+++ v3_website/index.php
@@ -0,0 +1,223 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>REUNA BUREAU</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Reuna is interactive agency that offers unique interactive experiences, custom installations, generative art and interactivity that capture the attention of people and invite them to interact." />
+    <meta name="author" content="Reuna Bureau" />
+    <!-- 1140px Grid styles for IE -->
+    <!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
+    <!-- CSS concatenated and minified via ant build script-->
+    <link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
+    <!-- end CSS-->
+    <script type="text/javascript" src="js/libs/css3-mediaqueries.js"></script> <!-- med-q for older browsers -->
+    <link rel="icon" type="image/gif" href="images/favicon.ico" />
+    <link rel="shortcut icon" href="images/favicon.ico" />
+</head>
+
+<body>
+<a name="landing" id="landing" class="page-link"></a>
+<div class="container landing-page">
+    <div class="row">
+        <div class="twelvecol slogan">
+            <canvas id="landing-canvas"></canvas>
+            <h1>We design and build unforgettable <a class="hilight" href="#interactivity">interactive experiences</a> that blend <a class="hilight" href="#surfaces">visually rich art</a> with <a class="hilight" href="#infovis">enlightening information</a>.</h1>
+            <h2>We are the masters of creative code</h2>
+        </div>
+    </div>
+    <div class="row shortinfo">
+        <div class="fourcol">
+            <h2>Multiple surfaces</h2>
+            <p>Break out of the box with the use of innovative surfaces &mdash; combinations of displays and projectors presenting captivating animation that reacts to the audience.</p>
+        </div>
+        <div class="fourcol">
+            <h2>Interactivity</h2>
+            <p>Engage your audience on a completely new level with new forms of interaction enabled through experimental sensors and computer vision.</p>
+        </div>
+        <div class="fourcol last">
+            <h2>Information Visualization</h2>
+            <p>Discover the hidden patterns in your data through beautifully flowing interactive visualizations. Gain insight into your business while impressing your customers and partners.</p>
+        </div>
+    </div>
+</div>
+
+<!-- top menu that sticks around -->
+<div class="container navigation">
+    <div class="row">
+        <div class="threecol logo">
+            <a href="#landing" id="landing-link"><img id="logo" src="images/reuna-logo-white.png" alt="Reuna" /></a>
+        </div>
+        <div class="ninecol last">
+            <nav>
+                <ul>
+                    <li><a href="#surfaces">Multiple surfaces</a></li>
+                    <li><a href="#interactivity">Interactivity</a></li>
+                    <li><a href="#infovis">Information visualization</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+
+
+<a name="surfaces" id="surfaces" class="page-link"></a>
+<div class="container">
+    <div class="row">
+        <div class="twelvecol">
+            <h1>Multiple surfaces</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="threecol">
+            <p>Combine an <b>unlimited</b> number of screens and projections to give your content the visibility it deserves.</p>
+            <p>Serve <b>any content</b> from videos to images to dynamic text and interactive visuals.</p>
+            <p>Capture the attention of your audience and lift central parts of your content up by <b>synchronously</b> playing them on all the screens.</p>
+            <p class="colorbox-video"><a href="http://player.vimeo.com/video/33879563?autoplay=1" class="colorbox-video">Watch the video</a></p>
+        </div>
+        <div class="ninecol last slider-wrapper theme-default">
+            <div class="ribbon"></div>
+            <div id="surfaces-carousel" class="nivoSlider">
+                <img src="images/contentflow-1.jpg" alt="" />
+                <img src="images/contentflow-2.jpg" alt="" />
+                <img src="images/contentflow-3.jpg" alt="" />
+                <a class="nivo-video-link" href="http://player.vimeo.com/video/33879563?autoplay=1">
+                    <img src="images/content-flow-screenie.png" alt="Watch the video" />
+                </a>
+            </div>
+            <!-- <div id="contentflow-video-htmlcaption" class="nivo-html-caption">
+                <span data-href="http://player.vimeo.com/video/33879563?autoplay=1">Watch the video</b>
+                (<a href="http://http://vimeo.com/33879563">vimeo link</a>)
+            </div> -->
+        </div>
+    </div>
+</div>
+
+<a name="interactivity" id="interactivity" class="page-link"></a>
+<div class="container">
+    <div class="row">
+        <div class="twelvecol">
+            <h1>Interactivity</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="threecol">
+            <p>Create much more memorable and deeper experiences through natural ways of interaction.</p>
+            <p class="mdash">&mdash;</p>
+            <h4>Example Project: Time Flow </h4>
+            <p>A beautiful projection displays temporal information interactively.</p>
+            <p>Participants control the display of information by moving within a space, each step taking them forward in time.</p>
+            <p>Include images and video to create depth and provide context.</p>
+            <p class="colorbox-video"><a href="http://player.vimeo.com/video/33879089?autoplay=1" class="colorbox-video">Watch the video</a></p>
+        </div>
+        <div class="ninecol last slider-wrapper theme-default">
+            <div class="ribbon"></div>
+            <div id="interactivity-carousel" class="nivoSlider">
+                <img src="images/timeflow-1.jpg" alt="" />
+                <img src="images/timeflow-2.jpg" alt="" />
+                <img src="images/timeflow-3.png" alt="" />
+                <img src="images/timeflow-4.png" alt="" />
+                <a class="nivo-video-link" href="http://player.vimeo.com/video/33879089?autoplay=1">
+                    <img src="images/time-flow-screenie.png" alt="Watch the video" />
+                </a>
+            </div>
+            <!-- <div id="timeflow-video-htmlcaption" class="nivo-html-caption">
+                <span data-href="http://player.vimeo.com/video/33879089?autoplay=1">Watch the video</b>
+                (<a href="http://http://vimeo.com/33879089">vimeo link</a>)
+            </div> -->
+        </div>
+    </div>
+</div>
+
+<a name="infovis" id="infovis" class="page-link"></a>
+<div class="container">
+    <div class="row">
+        <div class="twelvecol">
+            <h1>Information visualization</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="threecol">
+            <p>Gain new insight into your data through real-time visualizations.</p>
+            <p>Create ever-lasting impressions in your clientele and partner network.</p> 
+            <p>Add interaction to dive deeper into the information.</p>
+            <p class="mdash">&mdash;</p>
+            <h4>Example Project: Seismi</h4>
+            <p>Several different views provide continuously updating information about the earthquakes around the globe.</p>
+            <p><a href="http://www.seismi.org">Experience it online</a></p>
+        </div>
+        <div class="ninecol last slider-wrapper theme-default">
+            <div class="ribbon"></div>
+            <div id="infovis-carousel" class="nivoSlider">
+                <img src="images/seismi-0.png" alt="" />
+                <img src="images/seismi-1.png" alt="" />
+                <img src="images/seismi-2.png" alt="" />
+                <img src="images/seismi-3.png" alt="" />
+                <img src="images/seismi-4.png" alt="" />
+            </div>
+        </div>
+    </div>
+</div>
+
+<a name="contact" id="contact" class="page-link"></a>
+<div class="container contact-page">
+    <div class="row">
+        <div class="twelvecol">
+            <h2>We are</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="threecol">
+            <h3>REUNA</h3>
+            <p>We're always interested in wacky business proposals and meeting with great people.</p>
+            <p>Visit us on <a href="http://twitter.com/reunabureau">Twitter</a>, <a href="http://vimeo.com/reuna">Vimeo</a> or just directly contact us by mail or phone.</p>
+            <p><a href="mailto:info@reunabureau.com">info@reunabureau.com</a><br />
+                +358 40 722 5101</p>
+        </div>
+        <div class="sixcol reuna-logo" style="text-align:center;">
+            <img src="images/aboutcontact-1.png" alt="Reuna" />
+        </div>
+        <div class="threecol last">
+            <div class="person">
+                <h3>Lauri Kainulainen</h3>
+                <p><a href="mailto:lauri@reunabureau.com">lauri@reunabureau.com</a><br />
+                    +358 40 722 5101</p>
+            </div>
+            <div class="person">
+                <h3>G&ouml;kce Taskan</h3>
+                <p><a href="mailto:gokce@reunabureau.com">gokce@reunabureau.com</a><br />
+                    +358 44 5566630</p>
+            </div>
+            <div class="person">
+                <h3>Niko Knappe</h3>
+                <p><a href="mailto:niko@reunabureau.com">niko@reunabureau.com</a><br />
+                    +358 50 4418182</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+<!-- scripts concatenated and minified via ant build script-->
+<script type="text/javascript" src="js/mylibs/jquery.nivo.slider.pack.js"></script>
+<script type="text/javascript" src="js/mylibs/jquery.colorbox-min.js"></script>
+<script defer src="js/mylibs/canvas.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<!-- end scripts-->
+<?php 
+$ip = $_SERVER["SERVER_ADDR"];
+if ($ip == '::1' || $ip == '127.0.0.1') {
+    echo 'local';
+} else { ?>
+    <script type="text/javascript">
+      var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-27962815-1']); _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+<?php } ?>
+</body>
+</html>
